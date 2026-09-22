6e11eb5ff530747f7e177fd4cf9bfd3096728cf4
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-09-22 14:11:24 +0530

    file upload
    no

---
+++ Java/Java Programm/03_Tokens_In_Java/07_Floating_Point_Literal/Test6.java
@@ -0,0 +1,17 @@
+public class Test6 
+{
+	public static void main(String[] args) 
+	{
+		// double a = 0791; //Error
+
+		double b = 0791.0; 
+
+		double c = 0777;  
+
+		double d = 0Xdead; 
+		
+		// double e = 0Xdead.0; //Error
+
+		System.out.println(b+","+c+","+d);
+	}
+}
